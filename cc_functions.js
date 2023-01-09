c809ed2a5b9e7664c1f53fb78a8a1b44ad0e932e
Стас <seleznev-00@mail.ru>
2023-01-09 14:56:01 +0300

    add: enginescheme

--- cc_functions.js
+++ cc_functions.js
@@ -20,3 +20,59 @@ function background(color) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.closePath();
 }
+
+function camera() {
+  camera_coords.x += velocity.x;
+  camera_coords.y += velocity.y;
+  ctx.translate(-camera_coords.x, -camera_coords.y);
+}
+
+//-Функция отривоки массива или обьекта с встроенной функцией draw()
+//-The function of drawing an array or object with a built-in function draw()
+function render(data) {
+  try {
+    if (Array.isArray(data)) {
+      for (let i = 0; i < data.length; i++) {
+        data[i].draw();
+      }
+    } else {
+      data.draw();
+    }
+  } catch {
+    console.log("Error: " + JSON.stringify(data));
+  }
+}
+
+//-Функция фильтрации массива по параметру жизненного цикла объекта
+//-Filtering function by object lifecycle parameter
+function lifeCycle(data) {
+  if (Array.isArray(data)) {
+    for (let i = data.length - 1; i >= 0; i--) {
+      if (data[i].lifeCycle <= 0) {
+        data.splice(i, 1);
+      }
+    }
+  } else if (data.lifeCycle <= 0) {
+    data.draw = () => {};
+  }
+}
+
+//-Функция обработки столкновения двух кругов и вызова вложенной функции при регистрации
+//-The function of handling the collision of two circles and calling a nested function during registration
+function crash(ObjectOne, ObjectTwo, eventfunction) {
+  let dist = Math.hypot(ObjectOne.x - ObjectTwo.x, ObjectOne.y - ObjectTwo.y);
+  // console.log(dist);
+  if (dist < ObjectOne.size + ObjectTwo.size) {
+    eventfunction();
+  }
+}
+
+//-Функция обратная обратная crash()
+//-Reverse of crash() func
+function evade(ObjectOne, ObjectTwo, eventfunction) {
+  let dist = Math.hypot(ObjectOne.x - ObjectTwo.x, ObjectOne.y - ObjectTwo.y);
+  // console.log(dist);
+  if (dist > ObjectOne.size + ObjectTwo.size) {
+    eventfunction();
+  }
+}
